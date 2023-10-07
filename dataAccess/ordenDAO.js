684bb2a481390beb492bfee742299e5757c51c65
MiguelAngelDeLaRosa <miguel.delarosa229290@potros.itson.edu.mx>
2023-10-07 15:55:02 -0600

    creacion de DAOS
    pagoDAO y ordenDAO

---
+++ dataAccess/ordenDAO.js
@@ -0,0 +1,48 @@
+const Orden = require('../models/Orden');
+
+class OrdenDAO {
+    constructor(){};
+
+    static async crearOrden(ordenData){
+        try {
+            const orden = new Orden(ordenData);
+            return await orden.save();
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    static async actualizarOrden(id, ordenData){
+        try {
+            return await Orden.findByIdAndUpdate(id, ordenData, {new: true});
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    static async eliminarOrden(id){
+        try {
+            return await Orden.findByIdAndRemove(id);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    static async obtenerOrdenPorId(id){
+        try {
+            return await Orden.findById(id);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    static async obtenerOrdenes(){
+        try {
+            return await Orden.find();
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+module.exports = OrdenDAO;
